993361a3ffac6092f6184ded24ed3e19ff84cda1
David <josedago1163@gmail.com>
2022-12-04 04:01:56 -0500

    dejamos los archivos neesarios solamente

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { modal_inicio } from "./modal";
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+    <header>
+      <button>
+        Menu principal
+      </button>
+      <p>
+        Hola
+      </p>
+    </header>
+    </div>
+    )
+}
+
+export default App;
---
+++ src/modal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function modal_inicio(){
+    console.log("Hey")
+}
+
+export {modal_inicio}
